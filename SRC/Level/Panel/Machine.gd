2eee80264e366a6fcf2dd55499e4bd95cc8ea798
bertmad3400 <skrivtilbertram@gmail.com>
2022-05-04 13:12:28 +0200

    Base machine control

--- SRC/Level/Panel/Machine.gd
+++ SRC/Level/Panel/Machine.gd
@@ -3,13 +3,19 @@ extends Node2D
 
 export var size: Vector2
 
+signal picked_up(node)
+
 func _ready() -> void:
 	$Panel.rect_size = size * 50
 	$Area/Col.position = size * 25
 	$Area/Col.shape.extents = size*25
 
 
-
-
 func _on_Area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	pass
+	print("k")
+	if event.is_action("pickup"):
+		emit_signal("picked_up", self)
+
+
+func _on_Area_mouse_entered() -> void:
+	print("l")
